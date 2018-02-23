e36224c48e239f046c178050758e3234c95d5056
hsubeen <ghd-tnqls4962@hanmail.net>
2018-02-24 02:21:41 +0900

    listview border설정 완료, 간격 설정 필요

--- app/src/main/java/com/example/ghd_t/myapplication/AboutUserFragment.java
+++ app/src/main/java/com/example/ghd_t/myapplication/AboutUserFragment.java
@@ -1,21 +1,22 @@
 package com.example.ghd_t.myapplication;
 
 
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutUserFragment extends Fragment {
-
 
     public AboutUserFragment() {
         // Required empty public constructor
@@ -27,12 +28,16 @@ public class AboutUserFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        // fragment에서 findViewById 사용하기
         View view = inflater.inflate(R.layout.fragment_about_user, container, false);
-        //ImageView imageView = (ImageView) view.findViewById(R.id.user_profile);
+        ListView search_reservation_list = (ListView) view.findViewById(R.id.search_reservation);
 
-
-
+        String[] menu_item = {"내가 오픈한 클래스 예약 현황","다른 클래스 예약 현황"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getContext(),
+                R.layout.listview_setting,
+                menu_item
+        );
+        search_reservation_list.setAdapter(adapter);
 
         return view;
     }
