8df87ef6be88aad1d4ba688ac0a02c3c9f4c0d1e
hsubeen <ghd-tnqls4962@hanmail.net>
2018-02-25 01:58:26 +0900

    클래스 추가 탭 구현. 연락처에 자동하이픈 추가. 구글지도는 아직

--- app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
+++ app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
@@ -3,9 +3,13 @@ package com.example.ghd_t.myapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 /**
@@ -23,7 +27,12 @@ public class RegClassFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reg_class, container, false);
+        View view = inflater.inflate(R.layout.fragment_reg_class, container, false);
+
+        // 연락처 입력시 하이픈(-) 자동 입력.
+        EditText edit_phone = (EditText) view.findViewById(R.id.edit_phone);
+        edit_phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        return view;
     }
 
 }
