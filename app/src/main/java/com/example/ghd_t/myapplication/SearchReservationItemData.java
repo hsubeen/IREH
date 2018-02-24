f4f822416f96188b962e16b56bfb3760f5fa0c22
hsubeen <ghd-tnqls4962@hanmail.net>
2018-02-24 15:57:30 +0900

    custom listview로 변경

---
+++ app/src/main/java/com/example/ghd_t/myapplication/SearchReservationItemData.java
@@ -0,0 +1,21 @@
+package com.example.ghd_t.myapplication;
+
+/**
+ * Created by ghd-t on 2018-02-24.
+ */
+
+public class SaearchReservationItemData {
+    private String menu_title;
+
+    public SaearchReservationItemData(String menu_title) {
+        this.menu_title = menu_title;
+    }
+
+    public String getMenu_title() {
+        return menu_title;
+    }
+
+    public void setMenu_title(String menu_title) {
+        this.menu_title = menu_title;
+    }
+}
