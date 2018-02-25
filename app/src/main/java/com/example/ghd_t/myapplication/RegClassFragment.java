ca709639b6ba5f3892224f25f73af67a463cd49b
hsubeen <ghd-tnqls4962@hanmail.net>
2018-02-26 01:42:26 +0900

    주소 버튼 클릭 시 다음 우편번호 서비스 출력

--- app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
+++ app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
@@ -1,6 +1,7 @@
 package com.example.ghd_t.myapplication;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.telephony.PhoneNumberFormattingTextWatcher;
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 
@@ -34,11 +36,18 @@ public class RegClassFragment extends Fragment {
         EditText brand_name = (EditText) view.findViewById(R.id.edit_brand_name);
         EditText brand_web = (EditText) view.findViewById(R.id.edit_brand_web);
         EditText brand_phone = (EditText) view.findViewById(R.id.edit_phone);
-        EditText brand_address = (EditText) view.findViewById(R.id.edit_address);
+        ImageButton brand_address = (ImageButton) view.findViewById(R.id.edit_address);
 
         // 연락처 입력시 하이픈(-) 자동 입력.
         brand_phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
 
+        brand_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), DaumWebViewActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
