ca735573db3c1e4a11b27561c9614539054c5aad
hsubeen <ghd-tnqls4962@hanmail.net>
2018-02-26 00:15:27 +0900

    클래스 추가 탭에 주소검색 버튼 추가

--- app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
+++ app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,10 +30,19 @@ public class RegClassFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_reg_class, container, false);
 
+
+        EditText brand_name = (EditText) view.findViewById(R.id.edit_brand_name);
+        EditText brand_web = (EditText) view.findViewById(R.id.edit_brand_web);
+        EditText brand_phone = (EditText) view.findViewById(R.id.edit_phone);
+        EditText brand_address = (EditText) view.findViewById(R.id.edit_address);
+
         // 연락처 입력시 하이픈(-) 자동 입력.
-        EditText edit_phone = (EditText) view.findViewById(R.id.edit_phone);
-        edit_phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        brand_phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+
         return view;
     }
+
+
 
 }
