d37d2a03ec12a1091f4455f4b0bf4c343e31938f
hsubeen <ghd-tnqls4962@hanmail.net>
2018-03-03 19:49:07 +0900

    클래스 추가 프래그먼트에 spinner 추가. 레이아웃 수정

--- app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
+++ app/src/main/java/com/example/ghd_t/myapplication/RegClassFragment.java
@@ -13,15 +13,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import org.w3c.dom.Text;
 
 import java.io.File;
+import java.util.concurrent.TimeoutException;
+import java.util.zip.Inflater;
 
 
 /**
@@ -50,8 +56,6 @@ public class RegClassFragment extends Fragment {
         Log.v("알림","RegClassFragment의 onCreateView호출됨");
         View view = inflater.inflate(R.layout.fragment_reg_class, container, false);
 
-
-
         EditText brand_name = (EditText) view.findViewById(R.id.edit_brand_name);
         EditText brand_web = (EditText) view.findViewById(R.id.edit_brand_web);
         EditText brand_phone = (EditText) view.findViewById(R.id.edit_phone);
@@ -59,8 +63,34 @@ public class RegClassFragment extends Fragment {
         ImageButton brand_address = (ImageButton) view.findViewById(R.id.edit_address);
 
 
+        // 분야 선택하는 Spinner선언과 event listener 구현
+        final Spinner spinner_field = (Spinner) view.findViewById(R.id.spinner_field);
+
+        String[] str = getResources().getStringArray(R.array.spinnerArray);
+        final ArrayAdapter<String> adapter= new ArrayAdapter<String>(getContext(),R.layout.spinner_item,str);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_field.setAdapter(adapter);
+
+
+
+        spinner_field.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+               if(spinner_field.getSelectedItemPosition()>0){
+
+                   Log.v("알림",spinner_field.getSelectedItem().toString()+ "is selected");
+               }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
         // 연락처 입력시 하이픈(-) 자동 입력.
         brand_phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
 
         // 주소 입력 버튼 클릭 시 daum 우편 서비스 띄우는 webview 실행
         brand_address.setOnClickListener(new View.OnClickListener() {
@@ -70,8 +100,8 @@ public class RegClassFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+
         return view;
     }
-
-
 }
