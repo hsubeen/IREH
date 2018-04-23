5fc8dfecf56ab805e1ede46c26ec56c0a117fcc5
hsubeen <ghd-tnqls4962@hanmail.net>
2018-04-24 00:46:09 +0900

    GPS로 얻은 위도 경도 -> Geocoder로 주소알아내기

--- app/src/main/java/com/example/ghd_t/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/ghd_t/myapplication/HomeFragment.java
@@ -5,28 +5,47 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Signature;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+
+    private GPSInfo gps;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -42,6 +61,74 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         final ImageView img = (ImageView) view.findViewById(R.id.home_image);
+
+
+        Button btn_gps = view.findViewById(R.id.gps);
+        btn_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isPermission){
+                    callPermission();
+                    return;
+                }
+
+                gps = new GPSInfo(getContext());
+
+                // GPS 사용유무 가져오기
+                if (gps.isGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+
+                    Geocoder gCoder = new Geocoder(getContext(), Locale.getDefault());
+                    List<Address> addr = null;
+                    try{
+                        addr = gCoder.getFromLocation(latitude,longitude,1);
+                        Address a = addr.get(0);
+                        String temp = "";
+                        for (int i=0;i <= a.getMaxAddressLineIndex();i++) {
+                            Log.v("알림", "AddressLine(" + i + ")" + a.getAddressLine(i) + "\n");
+                        }
+                        Toast.makeText(getContext(),  a.getLocality() + " "  + " "+ a.getThoroughfare(), Toast.LENGTH_LONG).show();
+
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    if (addr != null) {
+                        if (addr.size()==0) {
+                            Toast.makeText(getContext(),"주소정보 없음", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                } else {
+                    // GPS 를 사용할수 없으므로
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+
+        callPermission();  // 권한 요청을 해야 함
+
+
+        // 분야 선택하는 Spinner선언과 event listener 구현
+        final Spinner spinner_field = (Spinner) view.findViewById(R.id.spinner_field);
+        String[] str = getResources().getStringArray(R.array.spinnerArray_forSearch);
+        final ArrayAdapter<String> adapter= new ArrayAdapter<String>(getContext(),R.layout.spinner_item,str);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_field.setAdapter(adapter);
+
+        spinner_field.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(spinner_field.getSelectedItemPosition()>0){
+                    Log.v("알림",spinner_field.getSelectedItem().toString()+ "is selected");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         final ListView home_brand_list = (ListView) view.findViewById(R.id.home_brandlist);
         ArrayList<BrandListItemData> data_brandlist = new ArrayList<>();
@@ -96,5 +183,50 @@ public class HomeFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            isAccessFineLocation = true;
+
+        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            isAccessCoarseLocation = true;
+        }
+
+        if (isAccessFineLocation && isAccessCoarseLocation) {
+            isPermission = true;
+        }
+    }
+
+    // 전화번호 권한 요청
+    private void callPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            requestPermissions(
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(
+                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
+        } else {
+            isPermission = true;
+        }
+    }
+
+
 
 }
